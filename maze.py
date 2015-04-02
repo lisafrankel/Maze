e05701ad8e26ea5d45ad9e00cbe3eba27354a9e0
Lisa Frankel <lisa@frankel.tv>
2015-04-01 20:10:32 -0400

    maze.py updated

--- maze.py
+++ maze.py
@@ -1,13 +1,28 @@
 # maze program by Lisa Frankel
 
+setMediaPath('/Users/Lisa/Desktop/College/4 Spring Term 2015/Computer Science 1 - Multimedia/Maze/')
+
+class Maze(object):
+  """ solves a maze """
+
+  def __init__(self):
+    """Initialization"""
+    self.image = makePicture("maze.jpg")
 
 # tests follow here
 
 doTests = 1
 if doTests:
-  # Test #1
+  # Test 1
   m = Maze()
   if m.__class__ == Maze:
     printNow("Test 1 passed, Maze exists.")
   else:
-    printNow("Test 1 failed, Maze does not exist")
+    printNow("Test 1 failed, Maze does not exist.")
+    
+  # Test 2
+  if m.image.__class__ == Picture:
+    printNow("Test 2 passed, Image exists.")
+  else:
+    printNow("Test 2 failed, Image doesn't exist.")
+
